d23c48aee3e6a41935768d74eebacc0a33a4c40e
Matthias Seifert <matthe.seifert@gmail.com>
2023-01-18 23:41:06 +0100

    extended deck view

---
+++ projekt-neuborkia/src/components/tcg/TcgCard.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+    export default {
+        props: {
+            cardName: String,
+            amount: Number
+        }
+    }
+</script>
+
+<template>
+    <div class="card-wrapper">
+        <img v-for="i in amount" 
+             :class="`card card-${i}`"
+             :src="`/src/assets/images/tcg/decks/${cardName}.png`" />
+    </div>
+</template>
+
+<style lang="stylus" scoped>
+    .card-wrapper
+        position relative
+        width calc(25% - 5em)
+
+        img.card
+            width 100%
+            border-radius .8em
+            box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+            transition all .1s
+
+            &:hover
+                transform scale(1.3)
+
+            &.card-2
+                position absolute
+                border-radius .7em
+                bottom -1em
+                z-index -1
+                width 95%
+                left 2.5%
+
+            &.card-3
+                position absolute
+                border-radius .6em
+                bottom -2em
+                z-index -2
+                width 90%
+                left 5%
+
+            &.card-4
+                position absolute
+                border-radius .4em
+                bottom -3em
+                width 85%
+                z-index -3
+                left 7.5%
+</style>
---
+++ projekt-neuborkia/src/components/tcg/TcgDeckInfo.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import TcgDeckBox from './TcgDeckBox.vue'
+    import TcgCard from './TcgCard.vue'
+
+    export default {
+        props: {
+            deck: {
+                type: Object,
+                required: true
+            }
+        },
+        components: {
+            TcgDeckBox,
+            TcgCard
+        }
+    }
+</script>
+
+<template>
+    <TcgDeckBox :deck="deck" :noLink="true" />
+    
+    <div class="card-list">
+        <TcgCard v-for="(amount, name) in deck.deckContent?.pokemon"
+                 :cardName="`${name}`"
+                 :amount="amount" />
+    </div>
+    <div class="card-list">
+        <TcgCard v-for="(amount, name) in deck.deckContent?.trainer"
+                 :cardName="`${name}`"
+                 :amount="amount" />
+    </div>
+    <div class="card-list energy">
+        <TcgCard v-for="(amount, name) in deck.deckContent?.energy"
+                 :cardName="`base_energy-${name}`"
+                 :amount="4" />
+    </div>
+</template>
+
+<style lang="stylus" scoped>
+    .card-list
+        margin 5em 0
+        display flex
+        flex-wrap wrap
+        justify-content space-between
+        gap 5em
+
+        &.energy
+            justify-content flex-start
+</style>
