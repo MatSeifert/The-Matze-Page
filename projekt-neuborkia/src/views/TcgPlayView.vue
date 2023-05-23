fc9aaf583f4494aa1d2c6a2c6667783aa20a78d6
Matthias Seifert <matthe.seifert@gmail.com>
2023-05-24 00:50:55 +0200

    completed basic functions for tcg playing

--- projekt-neuborkia/src/views/TcgPlayView.vue
+++ projekt-neuborkia/src/views/TcgPlayView.vue
@@ -18,7 +18,7 @@ export default {
                 asleep: false,
                 paralyzed: false,
                 confused: false
-            },
+            } as any,
             bench: [] as Object[],
             availablePowers: {
                 gx: true,
@@ -30,16 +30,38 @@ export default {
             STATUS_PARALYZED: 'paralyzed',
             STATUS_COFUSED: 'confused',
             POWER_GX: 'gx',
-            POWER_VSTAR: 'vstar'
+            POWER_VSTAR: 'vstar',
+            selectedDamageAdder: 10
         }
     },
     methods: {
-        addDamage(amount: number, position: number) {
+        addDamage(position: number, addDamage: boolean) {
+            const damageAmount = this.selectedDamageAdder
+
             if (position == -1) {
                 // Active Pokémon
-                this.active.damage += amount
+                if (addDamage) {
+                    this.active.damage += damageAmount
+                } else {
+                    if (this.active.damage - damageAmount < 0) {
+                        this.active.damage = 0
+                    } else {
+                        this.active.damage -= damageAmount
+                    }
+                }
             } else {
-                // add damage to benched pokémon at given position
+                if (!this.bench[position]) return
+                const relevantMon = this.bench[position] as any
+
+                if(addDamage) {
+                    relevantMon.damage += 10
+                } else {
+                    if (relevantMon.damage - damageAmount < 0) {
+                        relevantMon.damage = 0
+                    } else {
+                        relevantMon.damage -= damageAmount
+                    }
+                }
             }
         },
         toggleStatus(statusType: string) {
@@ -102,6 +124,43 @@ export default {
         },
         setActiveMon(mon: any) {
             this.active = mon
+        },
+        setDamageAdder(adder: number) {
+            this.selectedDamageAdder = adder
+        },
+        knockOut(position?: number) {
+            if (position !== undefined || position !== null) {
+                this.bench.splice(position as number, 1)
+            } else {
+                this.active = {
+                    damage: 0,
+                    poisoned: false,
+                    burned: false,
+                    asleep: false,
+                    paralyzed: false,
+                    confused: false
+                }
+
+                this.selectedDamageAdder = 10
+            }
+        },
+        switchOut(index: number) {
+            // cannot switch out, if the bench or the given spot is empty
+            if (!this.bench.length || !this.bench[index]) return
+
+            // Deep Clone, we dont want to store a reference
+            const temp = Object.assign({}, this.active)
+            this.active = Object.assign({}, this.bench[index])
+
+            // recovering from special conditions
+            temp.poisoned = false
+            temp.burned = false
+            temp.asleep = false
+            temp.paralyzed = false
+            temp.confused = false
+
+            this.bench.splice(index, 1)
+            this.bench.push(temp)
         }
     },
     components: {
@@ -119,8 +178,21 @@ export default {
 <template>
     <div class="play-area">
         <div class="active-area">
-            <div class="active-mon" @click="addDamage(10, -1)">
+            <div class="active-mon">
+                <div class="damage-bar">
+                    <div @click="addDamage(-1, false)">-</div>
+                    <div :class="selectedDamageAdder == 10 ? 'active' : ''" @click="setDamageAdder(10)">10</div>
+                    <div :class="selectedDamageAdder == 20 ? 'active' : ''" @click="setDamageAdder(20)">20</div>
+                    <div :class="selectedDamageAdder == 30 ? 'active' : ''" @click="setDamageAdder(30)">30</div>
+                    <div :class="selectedDamageAdder == 50 ? 'active' : ''" @click="setDamageAdder(50)">50</div>
+                    <div @click="addDamage(-1, true)">+</div>
+                </div>
                 <div class="damage">{{ active.damage }}</div>
+                <div class="action-bar">
+                    <div class="knocked-out" @click="knockOut()">
+                        besiegt
+                    </div>
+                </div>
             </div>
             <div class="active-mon-status">
                 <div :class="`status poisoned ${active.poisoned ? 'active' : ''}`" @click="toggleStatus(STATUS_POISONED)">
@@ -149,9 +221,21 @@ export default {
             </div>
         </div>
         <div class="bench">
-            <div class="benched-mon" v-for="b in 5">
+            <div :class="`benched-mon ${bench[b - 1] ? 'assigned' : 'empty'}`" v-for="b in 5">
                 <div v-if="bench[b - 1]">
-                    <span>{{ getBenchedInfo(b - 1, 'damage') }}</span>
+                    <div class="bench-action" @click="switchOut(b - 1)">
+                        tausch
+                    </div>
+
+                    <div class="bench-hp-bar">
+                        <div class="add-bench-damage substract" @click="addDamage(b - 1, false)">-</div>
+                        <span>{{ getBenchedInfo(b - 1, 'damage') }}</span>
+                        <div class="add-bench-damage add" @click="addDamage(b - 1, true)">+</div>
+                    </div>
+
+                    <div class="bench-action" @click="knockOut(b - 1)">
+                        besiegt
+                    </div>
                 </div>
                 <div v-else @click="addBenchedMon()">
                     +
@@ -175,10 +259,70 @@ export default {
                 border 1px solid rgba(255, 255, 255, .25)
                 background rgba(0, 0, 0, .5)
                 display flex
+                flex-direction column
                 align-items center
                 justify-content center
+                gap 1em
                 font-size 3em
                 font-weight 800
+
+                .damage-bar, .action-bar
+                    display flex
+                    font-size .3em
+                    font-weight 600
+                    border 1px solid rgba(255, 255, 255, .3)
+                    border-radius 2em
+                    overflow hidden
+
+                .damage-bar > div
+                    padding .7em
+                    border-right 1px solid rgba(255, 255, 255, .2)
+                    background rgba(0, 0, 0, .5)
+                    cursor pointer
+                    color rgba(255, 255, 255, .4)
+
+                    &.active
+                        background: linear-gradient(to bottom,#353535,#1e1e1e);
+                        color #fff
+
+                    &:first-child
+                        padding .7em .8em .7em 1em
+                        position relative
+                        color #ff0054
+
+                        &:before
+                            background #ff0054
+
+                    &:last-child
+                        padding .7em 1em .7em .8em
+                        border none 
+                        position relative
+                        color #00ff64
+
+                        &:before
+                            background #00ff64
+
+                    &:first-child:before, &:last-child:before
+                        content ''
+                        display block
+                        width 80%
+                        height 80%
+                        position absolute
+                        top 10%
+                        left 10%
+                        z-index -1
+                        filter blur(10px)
+                        opacity .8
+
+                .knocked-out, .switch-out
+                    flex 1
+                    color #fff
+                    text-transform uppercase
+                    padding .7em 1em
+                    cursor pointer
+
+                .knocked-out
+                    border-right 1px solid rgba(255, 255, 255, .2)
 
             .active-mon-status
                 display flex
@@ -312,12 +456,87 @@ export default {
                 height 7em
                 font-size 2.5em
                 font-weight 800
-                border-radius .5em
-                border 1px dashed rgba(255, 255, 255, .3)
-                background rgba(0, 0, 0, .15)
                 display flex
                 justify-content center
                 align-items center
-                color rgba(255, 255, 255, .3)
                 cursor pointer
+                border-radius .5em
+
+                > div
+                    display flex
+                    flex-direction column
+                    justify-content space-around
+                    align-items center
+                    height 100%
+                    width 100%
+
+                    .bench-action
+                        text-transform uppercase
+                        font-size .33em
+                        border 1px solid rgba(255, 255, 255, .2)
+                        display inline-block
+                        padding .7em 1em
+                        border-radius 2em
+
+                &.empty > div
+                    width 100%
+                    height 100%
+                    display flex
+                    align-items center
+                    justify-content center
+                    border-radius .5em
+                    border 1px dashed rgba(255, 255, 255, .3)
+                    background rgba(0, 0, 0, .15)
+                    color rgba(255, 255, 255, .3)
+
+                &.assigned
+                    border 1px solid rgba(255, 255, 255, .3)
+                    background rgba(0, 0, 0, .5)
+
+                    .bench-hp-bar
+                        display flex
+                        align-items center
+                        justify-content center
+                        gap .25em
+                        width 100%
+                    
+                        .add-bench-damage
+                            width 2em
+                            height 2em
+                            border 1px solid rgba(255, 255, 255, .2)
+                            border-radius 50%
+                            display flex
+                            align-items center
+                            justify-content center
+                            font-size .5em
+                            cursor pointer
+
+                            &.add, &.substract
+                                position relative
+                                overflow hidden
+
+                                &:before
+                                    content ''
+                                    display block
+                                    width 60%
+                                    height 60%
+                                    z-index -1
+                                    border-radius 50%
+                                    position absolute
+                                    left 20%
+                                    top 20%
+                                    filter blur(15px)
+                                    opacity .5
+
+                            &.add
+                                color #00ff64
+
+                                &:before
+                                    background #00ff64
+
+                            &.substract
+                                color #ff0054
+
+                                &:before
+                                    background #ff0054
 </style>
