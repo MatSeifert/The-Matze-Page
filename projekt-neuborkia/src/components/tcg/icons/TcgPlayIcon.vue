e3775cf1c6c39e1e27a5c33488aabcaf49d38447
Matthias Seifert <matthe.seifert@gmail.com>
2023-05-24 17:02:56 +0200

    finished virtual playmat

---
+++ projekt-neuborkia/src/components/tcg/icons/TcgPlayIcon.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+    export default {
+        props: {
+            fill: String,
+            stroke: String
+        }
+    }
+</script>
+
+<template>
+    <svg width="100%" height="100%" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;">
+        <path d="M52,17L48,7L40,30L17,21L28,43L5,52L11,54" style="fill:none;stroke:white;stroke-width:4px;"/>
+        <g transform="matrix(-1,0,0,-1,98,101.5)">
+            <path d="M52,17L48,7L40,30L17,21L28,43L5,52L11,54" style="fill:none;stroke:white;stroke-width:4px;"/>
+        </g>
+        <g transform="matrix(1.14286,0,0,1.14286,-7.85714,-5.42857)">
+            <circle cx="65.5" cy="34.5" r="3.5" style="fill:none;stroke:white;stroke-width:2.62px;"/>
+        </g>
+        <g transform="matrix(1.14286,0,0,1.14286,-44.3571,28.0714)">
+            <circle cx="65.5" cy="34.5" r="3.5" style="fill:none;stroke:white;stroke-width:2.62px;"/>
+        </g>
+        <path d="M75.126,36.5L82.01,36.5C80.708,43.883 74.255,49.5 66.5,49.5C58.745,49.5 52.292,43.883 50.99,36.5L58.874,36.5C59.943,39.973 63.178,42.5 67,42.5C70.822,42.5 74.057,39.973 75.126,36.5Z" style="fill:none;stroke:white;stroke-width:3px;"/>
+        <g transform="matrix(1,0,0,1,-36.5,33.5)">
+            <path d="M75.126,36.5L82.01,36.5C80.708,43.883 74.255,49.5 66.5,49.5C58.745,49.5 52.292,43.883 50.99,36.5L58.874,36.5C59.943,39.973 63.178,42.5 67,42.5C70.822,42.5 74.057,39.973 75.126,36.5Z" style="fill:none;stroke:white;stroke-width:3px;"/>
+        </g>
+        <g transform="matrix(1,0,0,-1,0,67)">
+            <path d="M75.126,36.5L82.01,36.5C80.708,43.883 74.255,49.5 66.5,49.5C58.745,49.5 52.292,43.883 50.99,36.5L58.874,36.5C59.943,39.973 63.178,42.5 67,42.5C70.822,42.5 74.057,39.973 75.126,36.5Z" style="fill:white;stroke:white;stroke-width:3px;"/>
+        </g>
+        <g transform="matrix(1,0,0,-1,-36.5,100.5)">
+            <path d="M75.126,36.5L82.01,36.5C80.708,43.883 74.255,49.5 66.5,49.5C58.745,49.5 52.292,43.883 50.99,36.5L58.874,36.5C59.943,39.973 63.178,42.5 67,42.5C70.822,42.5 74.057,39.973 75.126,36.5Z" style="fill:white;stroke:white;stroke-width:3px;"/>
+        </g>
+    </svg>
+</template>
