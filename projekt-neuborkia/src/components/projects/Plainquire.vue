e934823f76e94ab7e78059fd7b2451b353835955
Matthias Seifert <matthe.seifert@gmail.com>
2024-06-30 18:49:18 +0200

    added latest project

--- projekt-neuborkia/src/components/projects/Plainquire.vue
+++ projekt-neuborkia/src/components/projects/Plainquire.vue
@@ -20,12 +20,23 @@ export default {
 <template>
   <ProjectHeader :projectName="projectName"
                  :headline="'Design der Landing Page und Website'"
-                 :subHeadline="'Tool zum Schreiben von SQL Anfragen direkt im Code'"
-                 :abstract="'Mit Plainquire wird die Datenabfrage aus angebundenen Datenbanken spielend einfach. Statt mühsam Code in Datenabfragen zu übersetzen kann mit Plainquire direkt im Code eine Query zusammengebaut werden, in der auch direkt gefiltert oder sortiert werden kann.'"
+                 :subHeadline="'Plainquire – C# Software Bibliothek'"
+                 :abstract="'Mit Plainquire wird die Datenabfrage aus angebundenen Datenbanken spielend einfach. Statt mühsam Code in Datenabfragen zu übersetzen kann mit Plainquire direkt im Code eine Query zusammengebaut werden, in der beispielsweise auch direkt gefiltert oder sortiert werden kann.'"
                  :timespan="'2 Monate'"
                  :year="'2024'"
                  :tags="tags"
-                 :linkTarget="'https://www.plainquire.com/'" />
+                 :linkTarget="'https://www.plainquire.com/'"
+                 :linkTargetSecondary="'https://github.com/plainquire/plainquire'"
+                 :buttonLabelSecondary="'Github'" />
+               
+  <Masonry :projectName="projectName" :imageCount="4" :fileType="'webp'"/>
+
+  <RouterLink
+    :to="{  path: '/', hash: '#projects' }"
+    class="no-hover-effect block text-center"
+  >
+    <button class="btn btn-primary">Zurück zur Übersicht</button>
+  </RouterLink>
 </template>
 
 <style scoped lang="stylus">
